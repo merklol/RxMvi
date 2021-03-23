de4158ba97af35749b28925bc836c1075e7b32a3
maximsmolyakov <sm.maksim@icloud.com>
2021-03-23 10:04:50 +0800

    feat: add RxMviFragment.kt
    This commit adds a workaround RxMviFragment class that all Fragments should extend from.

---
+++ rxmvi/src/main/java/com/maximcode/rxmvi/view/RxMviFragment.kt
@@ -0,0 +1,55 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 Maxim Smolyakov
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.maximcode.rxmvi.view
+
+import androidx.annotation.LayoutRes
+import androidx.fragment.app.Fragment
+
+/**
+ * A fragment that implements the [View] interface to bind and unbind it to the store.
+ *
+ * Note: All Fragments should extend this to get RxMvi functionality.
+ */
+public abstract class RxMviFragment<State, ViewModel: RxMviViewModel<State>>: Fragment, View<State> {
+    public constructor(): super()
+
+    public constructor(@LayoutRes contentLayoutId: Int): super(contentLayoutId)
+
+    public abstract val viewModel: ViewModel
+
+    /**
+     * Renders the state of the store to the UI
+     */
+    abstract override fun render(state: State)
+
+    override fun onPause() {
+        super.onPause()
+        viewModel.unbind()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        viewModel.bind(this)
+    }
+}
